32e6227fe4aae9e490694ffa3fc7714a0f1bf91e
Edinei Lucas Silva Souza <edineilucas001@gmail.com>
2025-11-03 13:36:13 -0300

    corrigido erro na funcao de adicionar aresta e na funcao imprime grafo. Adicionado todas as arestas

--- lista de adjacencia.c
+++ lista de adjacencia.c
@@ -57,32 +57,56 @@ Grafo* insere_vertice(Grafo * G, int x){
     return G;
 }
 
-Grafo* insere_aresta_direcionda(Grafo *G, int id_saida, int id_destino, int peso){
-    Grafo * p = busca_vertice(G,id_saida);
-    Aresta * novo_vizinho = (Aresta*)malloc(sizeof(Aresta));
-    novo_vizinho->id_vizinho = id_destino;
-    novo_vizinho->peso = peso;
-    novo_vizinho->proximo = G->prim_vizinho;
-    p->prim_vizinho = novo_vizinho;
+void insere_aresta_direcionada(Grafo * g, int id_saida, int id_destino, int peso){
+    Grafo * v = busca_vertice(g, id_saida);
+    Aresta * nova_e = (Aresta*)malloc(sizeof(Aresta));
+    nova_e->id_vizinho = id_destino;
+    nova_e->peso = peso;
+    nova_e->proximo = v->prim_vizinho;
+    v->prim_vizinho = nova_e;
+    return;
 }
 
 void imprimir_grafo(Grafo * G){
-    materias * s ;
+    printf("Lista de adjacencia:\n");
     while(G!= NULL){
-        s = busca_nome(G->ID);
-        printf("Materia: %s", s->nome);
-        Aresta *a = G->prim_vizinho;
-        while(a != NULL){
-            s = busca_nome(a->id_vizinho);
-            printf("->(%s Peso:%d)", s->nome, a->peso);
-            if(a->proximo!= NULL){
+        materias * v ;
+        v = busca_nome(G->ID);
+        printf("%s", v->nome);
+        if(G->prim_vizinho != NULL){
+            printf("->");
+        }
+        Aresta * e = G->prim_vizinho;
+        while(e != NULL){
+            materias * e2 = busca_nome(e->id_vizinho); 
+            printf("(%s Peso:%d)",e2->nome, e->peso);
+            if(e->proximo!= NULL){
                 printf("->");
             }
-            a = a->proximo;
+            e = e->proximo;
         }
         printf("\n");
         G = G->prox;
     }
+}
+
+void libera_arestas(Aresta * e){
+    while (e != NULL){
+        Aresta * rem = e;
+        e = e->proximo;
+        free(rem);
+    }
+    return;
+}
+
+void libera_vertices(Grafo * v){
+    while (v != NULL){
+        libera_arestas(v->prim_vizinho);
+        Grafo *rem = v;
+        v = v->prox;
+        free(rem);
+    }
+    return;
 }
 
 int main (){
@@ -91,6 +115,50 @@ int main (){
     for (int i = 51; i>=0; i--){
         g = insere_vertice(g, m[i].ID);
     }
+
+    insere_aresta_direcionada(g, 14066, 14068, 1);
+    insere_aresta_direcionada(g, 14064, 14069, 1);
+    insere_aresta_direcionada(g, 14064, 14076, 1);
+    insere_aresta_direcionada(g, 14064, 14085, 1);
+    insere_aresta_direcionada(g, 14065, 14071, 1);
+    insere_aresta_direcionada(g, 14067, 14072, 1);
+    insere_aresta_direcionada(g, 14068, 14075, 1);
+    insere_aresta_direcionada(g, 14069, 14082, 1);
+    insere_aresta_direcionada(g, 14071, 14087, 1);
+    insere_aresta_direcionada(g, 14071, 14098, 1);
+    insere_aresta_direcionada(g, 14071, 14105, 1);
+    insere_aresta_direcionada(g, 14073, 14078, 1);
+    insere_aresta_direcionada(g, 14079, 14096, 1);
+    insere_aresta_direcionada(g, 14074, 14079, 1);
+    insere_aresta_direcionada(g, 14074, 14091, 1);
+    insere_aresta_direcionada(g, 14074, 14101, 1);
+    insere_aresta_direcionada(g, 14075, 14080, 1);
+    insere_aresta_direcionada(g, 14075, 14084, 1);
+    insere_aresta_direcionada(g, 14075, 14086, 1);
+    insere_aresta_direcionada(g, 14075, 14087, 1);
+    insere_aresta_direcionada(g, 14075, 14092, 1);
+    insere_aresta_direcionada(g, 14075, 14094, 1);
+    insere_aresta_direcionada(g, 14075, 14098, 1);
+    insere_aresta_direcionada(g, 14075, 14104, 1);
+    insere_aresta_direcionada(g, 14075, 14103, 1);
+    insere_aresta_direcionada(g, 14075, 14105, 1);
+    insere_aresta_direcionada(g, 14076, 14081, 1);
+    insere_aresta_direcionada(g, 14078, 14083, 1);
+    insere_aresta_direcionada(g, 14080, 14091, 1);
+    insere_aresta_direcionada(g, 14083, 14089, 1);
+    insere_aresta_direcionada(g, 14084, 14088, 1);
+    insere_aresta_direcionada(g, 14084, 14090, 1);
+    insere_aresta_direcionada(g, 14086, 14097, 1);
+    insere_aresta_direcionada(g, 14088, 14093, 1);
+    insere_aresta_direcionada(g, 14089, 14095, 1);
+    insere_aresta_direcionada(g, 14089, 14099, 1);
+    insere_aresta_direcionada(g, 14091, 14096, 1);
+    insere_aresta_direcionada(g, 14091, 14102, 1);
+    insere_aresta_direcionada(g, 14092, 14097, 1);
+    insere_aresta_direcionada(g, 14095, 14104, 1);
+    insere_aresta_direcionada(g, 14101, 14104, 1);
+    
     imprimir_grafo(g);
+    
     return 0;
 }
