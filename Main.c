7e6ce8561cad0b9b1662750a8fcdca0020764e66
Edinei Lucas Silva Souza <edineilucas001@gmail.com>
2025-11-25 20:08:10 -0300

    Implementado interacao com o usuario

--- Main.c
+++ Main.c
@@ -7,15 +7,23 @@
 int main (){
 
     Grafo * g = inicializa_Grafo();
+
     for (int i = 51; i>=0; i--){
         g = insere_vertice(g, m[i].ID);
     }
 
     insere(g);
 
-    imprimir_grafo(g);
+    //imprimir_grafo(g);
 
-    int alvo = 14104;
+    printf("lista de materia:\n");
+    for (int i = 0; i<max_vertices; ++i){
+        printf("Nome:%s ID:%d Periodo:%d\n", m[i].nome, m[i].ID, m[i].per);
+    }
+
+    int alvo = 0;
+    printf("insira o codigo da meteria desejada:");
+    scanf("%d", &alvo);
     materias *alv = busca_nome(alvo);
     printf("\nColetando precedentes de %d/%s\n", alvo, alv->nome);
     coleta_precedentes(g, alvo);
